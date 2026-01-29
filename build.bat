ed035491c625fce3cc38922e913a237654a66e45
lpego <luca.pegoraro90@gmail.com>
2026-01-29 15:15:02 +0100

    /f was mangling spaces... Converting to JSON-safe parsing

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Load environment variables from .secret file
-for /f "delims== tokens=1,2" %%A in (.secret) do set %%A=%%B
+for /f "usebackq delims=" %%A in (".secret") do set "%%A"
 
-@REM REM Run the build and serve commands
-@REM Rscript _preprocess.R && Rscript -e "blogdown::build_site(build_rmd = TRUE)" && Rscript -e "blogdown::serve_site()"
+REM Run the build and serve commands
+Rscript _preprocess.R && Rscript -e "blogdown::build_site(build_rmd = TRUE)" && Rscript -e "blogdown::serve_site()"
